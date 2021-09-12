823e4d93eef51705a16749c18598662176fbf433
Sameedeveloper <84496887+Sameedeveloper@users.noreply.github.com>
2021-09-12 23:21:05 +0500

    Add files via upload

--- js/carousel.js
+++ js/carousel.js
@@ -13,3 +13,14 @@ items.forEach((el) => {
     next = next.nextElementSibling
   }
 })
+
+function PopUp(hideOrshow) {
+  if (hideOrshow == 'hide')
+    document.getElementById('ac-wrapper').style.display = 'none'
+  else document.getElementById('ac-wrapper').removeAttribute('style')
+}
+window.onload = function () {
+  setTimeout(function () {
+    PopUp('show')
+  }, 500)
+}
